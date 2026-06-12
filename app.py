0835d84137ee9d0881925e8e9aa786ee54861f77
Vitin <extroierr@gmail.com>
2026-06-12 03:55:22 -0300

    fix: usar threading mode compativel com Python 3.14

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'rpg-mesa-secret-2024')
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 # Estado das salas: {room_id: {players, map_state, tokens, messages}}
 rooms = {}
